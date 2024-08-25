e84e130d330ee9031ef01d26c65707f96f3441aa
msritahmed <166241081+msritahmed@users.noreply.github.com>
2024-08-25 19:24:52 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,17 @@
+
+// area program
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+    float a,b;
+    printf("enter the  parameters:\n");
+    scanf("%f%f",&a,&b);
+    
+    printf("area of triangle:%f\n",(0.5*a*b));
+    printf("area of rectagle:%f\n",(a*b));
+     printf("area of square:%f\n",(a*a));
+      printf("area of circle:%f\n",(3.14*a*a));
+    
+}
