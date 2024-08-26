b547ed259c92260812100f9f90bab6ec755f9d28
msritahmed <166241081+msritahmed@users.noreply.github.com>
2024-08-26 16:32:57 +0530

    Create swapping 2 numbers without temp.c

---
+++ swapping 2 numbers without temp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+void main()
+{
+    int a,b;
+    printf("enter the numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("before swapping %d and %d\n",a,b);
+    a=a^b;
+     b=a^b;
+      a=a^b;
+      printf("after swapping %d and %d\n",a,b);
+      
+}
